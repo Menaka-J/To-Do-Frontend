e49e92fcbfd1bf42abc7331cf2c5a3db093798b5
Menaka J <menakaraman4@gmail.com>
2026-01-05 23:55:20 +0530

    cm2

--- todo-frontend/src/Todo.jsx
+++ todo-frontend/src/Todo.jsx
@@ -1,6 +1,17 @@
 function Todo() {
     return (
         <>
+            <div className="row p-3 bg-success text-light mb-4">
+                <h1>ToDo App</h1>
+            </div>
+            <div className="row">
+                <h3>Add item</h3>
+                <p className="text-success">Item added successfully</p>
+                <div className="form-group d-flex gap-2">
+                    <input type="text" className="form-control" />
+                    <button className="btn btn-dark">Submit</button>
+                </div>
+            </div>
         </>
     )
 }
