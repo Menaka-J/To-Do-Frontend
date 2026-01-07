92588c8365bbd9cff6ab804c01341f7f8017ac38
Menaka J <menakaraman4@gmail.com>
2026-01-07 21:55:45 +0530

    commit3

--- todo-frontend/src/Todo.jsx
+++ todo-frontend/src/Todo.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react"
+
 function Todo() {
+    const [title,setTitle] = useState("");
+    const [description, sestDescription] = useState("");
+
+    function handlesubmit() {
+
+    }
     return (
         <>
             <div className="row p-3 bg-success text-light mb-4">
                 <h1>ToDo App</h1>
             </div>
-            <div className="row">
+            <div className="column">
                 <h3>Add item</h3>
                 <p className="text-success">Item added successfully</p>
                 <div className="form-group d-flex gap-2">
-                    <input type="text" className="form-control" />
-                    <button className="btn btn-dark">Submit</button>
+                    <input type="text" className="form-control" placeholder="Title" value={title}/>
+                    <input type="text" className="form-control" placeholder="Description" value={description}/>
+                    <button className="btn btn-dark" onClick={handlesubmit}>Submit</button>
                 </div>
             </div>
         </>
