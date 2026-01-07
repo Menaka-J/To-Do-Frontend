a0dad8fefbd1afae656a0eb3ac0a04d22b00237f
Menaka J <menakaraman4@gmail.com>
2026-01-07 23:33:13 +0530

    commit9

--- todo-frontend/src/Todo.jsx
+++ todo-frontend/src/Todo.jsx
@@ -6,8 +6,14 @@ function Todo() {
     const [todos, setTodos] = useState([]);
     const [error, setsError] = useState("");
     const [message, setmessage] = useState("");
+    //edit
+    const [editid, setEditid] = useState(-1);
+    const [editTitle, setEdittitle] = useState("");
+    const [editDesc, setEditdesc] = useState("");
+
     const apiURL = "http://localhost:8000";
 
+    //===========================================================================================
     //function for adding item
     function handlesubmit() {
 
@@ -40,6 +46,9 @@ function Todo() {
         }
     }
 
+
+    //===========================================================================================
+    //getting items
     useEffect(() => {
         getitems();
     }, [])
@@ -52,45 +61,201 @@ function Todo() {
             })
     }
 
+    //===========================================================================================
+    //updating items
+    function handleedit(item) {
+        setEditid(item._id);
+        setEdittitle(item.title);
+        setEditdesc(item.description);
+    }
+
+    function handleupdate() {
+        setsError("");
+        //check inputs
+        if (editTitle.trim() != "" && editDesc.trim() != "") {
+            fetch(apiURL + "/todos/" + editid, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ title: editTitle, description: editDesc })
+            }).then((res) => {
+                if (res.ok) {
+                    //update item 
+                    const updatedtodo = todos.map((item) =>
+                        item._id === editid
+                            ? { ...item, title: editTitle, description: editDesc }
+                            : item
+                    );
+
+                    setTodos(updatedtodo);
+                    setmessage("Item updated successfully");
+                    setTimeout(() => {
+                        setmessage("");
+                        setTitle("");
+                        sestDescription("");
+                    }, 3000);
+
+                    setEditid(-1);
+                } else {
+                    //show error
+                    setsError("Unable to update todo item");
+                }
+            }).catch(() => {
+                setsError("OOPS!! Unable to update todo item");
+            })
+        }
+    }
+
+    //===========================================================================================
+    //deleting item
+    function handledelete(itemid) {
+        if (window.confirm("You want to delete task?")) {
+            fetch(apiURL + "/todos/" + itemid, {
+                method: "DELETE"
+            })
+                .then(() => {
+                    const newtodos = todos.filter((item) => item._id !== itemid);
+                    setTodos(newtodos);
+                })
+        }
+    }
+
     return (
         <>
-            <div className="row p-3 bg-success text-light mb-4">
-                <h1>ToDo App</h1>
-            </div>
 
-            {/* creating item  */}
-            <div className="column">
-                <h3>Add item</h3>
-                {message && <p className="text-success">{message}</p>}
-                <div className="form-group d-flex px-2">
-                    <input type="text" className="form-control mr-3" placeholder="Title" value={title} onChange={(e) => { setTitle(e.target.value) }} />
-                    <input type="text" className="form-control mr-2" placeholder="Description" value={description} onChange={(e) => { sestDescription(e.target.value) }} />
-                    <button className="btn btn-dark ml-2" onClick={handlesubmit}>Submit</button>
+            <div className="column card p-4 shadow-sm mb-4 bg-gray m-3">
+                {/* title  */}
+                <div className="row bg-success text-light mb-4 rounded shadow-sm">
+                    <div className="col py-3 px-4 d-flex flex-column justify-content-center">
+                        <h1 className="font-weight-bold mb-1">📝 ToDo App</h1>
+                        <p className="mb-0 font-italic small">Plan • Track • Complete</p>
+                    </div>
                 </div>
-                {error && <p className="text-danger">{error}</p>}
-            </div>
 
-            {/* getting items  */}
-            <div className="column mt-3">
-                <h3>Tasks</h3>
-                <ul className="list-group">
-                    {todos.map((item) => (
-                        <li key={item._id} className="list-group-item d-flex bg-info justify-content-between align-items-center my-2">
-                            <div className="d-flex flex-column">
-                                <span className="font-weight-bold">{item.title}</span>
-                                <span>{item.description}</span>
-                            </div>
-                            <div className="d-flex px-2">
-                                <button className=" btn btn-warning mr-2">Edit</button>
-                                <button className="btn btn-danger">Delete</button>
-                            </div>
-                        </li>
-                    ))}
+                {/* creating item  */}
+                <div className="column card p-4 shadow-sm mb-4 bg-gray">
+                    <h3>Add item</h3>
+                    {message && <p className="text-success">{message}</p>}
+                    <div className="form-group d-flex px-2">
+                        <input type="text" className="form-control mr-3" placeholder="Title" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+                        <input type="text" className="form-control mr-2" placeholder="Description" value={description} onChange={(e) => { sestDescription(e.target.value) }} />
+                        <button className="btn btn-dark ml-2" onClick={handlesubmit}>Submit</button>
+                    </div>
+                    {error && <p className="text-danger">{error}</p>}
+                </div>
 
-                </ul>
+                {/* task list items  */}
+                <div className="column mt-3">
+                    <h3>Tasks</h3>
+
+                    {/* getting items  */}
+                    <ul className="list-group">
+                        {todos.map((item) => (
+                            <li key={item._id} className="list-group-item d-flex bg-info justify-content-between align-items-center my-2">
+                                <div className="d-flex flex-column">
+                                    {
+                                        editid == -1 || editid !== item._id ?
+                                            <>
+                                                <span className="font-weight-bold">{item.title}</span>
+                                                <span>{item.description}</span>
+                                            </> : <>
+                                                <div className="form-group d-flex px-2">
+                                                    <input type="text" className="form-control mr-3" placeholder="Title" value={editTitle} onChange={(e) => { setEdittitle(e.target.value) }} />
+                                                    <input type="text" className="form-control mr-2" placeholder="Description" value={editDesc} onChange={(e) => { setEditdesc(e.target.value) }} />
+                                                </div>
+                                            </>
+                                    }
+
+                                </div>
+                                {/* button edit,update and delete,edit  */}
+                                {/* updating items and deleting items  */}
+                                <div className="d-flex px-2">
+                                    {
+                                        editid == -1 || editid !== item._id ?
+                                            <>{/* edit and delete button*/}
+                                                <button className=" btn btn-warning mr-2" onClick={() => handleedit(item)}>Edit</button>
+                                                <button className="btn btn-danger" onClick={() => handledelete(item._id)}>Delete</button>
+                                            </> :
+                                            <>{/*cancel and update button  */}
+                                                <button className=" btn btn-warning mr-2" onClick={() => handleupdate(item._id)}>Update</button>
+                                                <button className="btn btn-danger" onClick={() => setEditid(-1)}>Cancel</button>
+                                            </>
+                                    }
+                                </div>
+                            </li>
+                        ))}
+
+                    </ul>
+                </div>
             </div>
+ 
         </>
     )
 }
 
 export default Todo
+
+// {/* title  */}
+//             <div className="row bg-success text-light mb-4 rounded shadow-sm">
+//                 <div className="col py-3 px-4 d-flex flex-column justify-content-center">
+//                     <h1 className="font-weight-bold mb-1">📝 ToDo App</h1>
+//                     <p className="mb-0 font-italic small">Plan • Track • Complete</p>
+//                 </div>
+//             </div>
+
+//             {/* creating item  */}
+//             <div className="column card p-4 shadow-sm mb-4 bg-gray">
+//                 <h3>Add item</h3>
+//                 {message && <p className="text-success">{message}</p>}
+//                 <div className="form-group d-flex px-2">
+//                     <input type="text" className="form-control mr-3" placeholder="Title" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+//                     <input type="text" className="form-control mr-2" placeholder="Description" value={description} onChange={(e) => { sestDescription(e.target.value) }} />
+//                     <button className="btn btn-dark ml-2" onClick={handlesubmit}>Submit</button>
+//                 </div>
+//                 {error && <p className="text-danger">{error}</p>}
+//             </div>
+
+//             {/* task list items  */}
+//             <div className="column mt-3">
+//                 <h3>Tasks</h3>
+
+//                 {/* getting items  */}
+//                 <ul className="list-group">
+//                     {todos.map((item) => (
+//                         <li key={item._id} className="list-group-item d-flex bg-info justify-content-between align-items-center my-2">
+//                             <div className="d-flex flex-column">
+//                                 {
+//                                     editid == -1 || editid !== item._id ?
+//                                         <>
+//                                             <span className="font-weight-bold">{item.title}</span>
+//                                             <span>{item.description}</span>
+//                                         </> : <>
+//                                             <div className="form-group d-flex px-2">
+//                                                 <input type="text" className="form-control mr-3" placeholder="Title" value={editTitle} onChange={(e) => { setEdittitle(e.target.value) }} />
+//                                                 <input type="text" className="form-control mr-2" placeholder="Description" value={editDesc} onChange={(e) => { setEditdesc(e.target.value) }} />
+//                                             </div>
+//                                         </>
+//                                 }
+
+//                             </div>
+//                             {/* button edit,update and delete,edit  */}
+//                             {/* updating items and deleting items  */}
+//                             <div className="d-flex px-2">
+//                                 {
+//                                     editid == -1 || editid !== item._id ?
+//                                         <>{/* edit and delete button*/}
+//                                             <button className=" btn btn-warning mr-2" onClick={() => handleedit(item)}>Edit</button>
+//                                             <button className="btn btn-danger" onClick={() => handledelete(item._id)}>Delete</button>
+//                                         </> :
+//                                         <>{/*cancel and update button  */}
+//                                             <button className=" btn btn-warning mr-2" onClick={() => handleupdate(item._id)}>Update</button>
+//                                             <button className="btn btn-danger" onClick={() => setEditid(-1)}>Cancel</button>
+//                                         </>
+//                                 }
+//                             </div>
+//                         </li>
+//                     ))}
+
+//                 </ul>
+//             </div>
